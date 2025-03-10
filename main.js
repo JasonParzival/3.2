1b26d9d86f37e04ca378cfb8da4b8246ba606b25
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-10 22:18:00 +0800

    Задание выполнено

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 async function process() {
     let r = await fetch("/data.json");
     let data = await r.json();
-    console.log(data) // выведем исходный список
+    console.log(data) 
     
     let mappedData = data.map(item => {
         return item['Насколько курс был полезен?'];
-    }) // мэпю
-    console.log(mappedData)  // выведем отображенный список
+    })
+    console.log(mappedData)
 
     let uniqueValues = [...new Set(mappedData)]
     console.log(uniqueValues)
@@ -15,13 +15,56 @@ async function process() {
     console.log(select)
 
     uniqueValues.forEach(item => {
-        // создаем в памяти элемент выпадающего списка
         let option = document.createElement("option"); 
 
-        option.value = item;  // устанавливаем значение элемента
-        option.text = item; // устанавливаем текст элемента, он совпадает со значением
+        option.value = item; 
+        option.text = item;
 
-        select.add(option) // добавляем элемент к выпадающему списку
+        select.add(option) 
+    })
+
+    //----------------------
+
+    mappedData = data.map(item => {
+        return item['Насколько доволен форматом обучения?'];
+    })
+    console.log(mappedData)
+
+    uniqueValues = [...new Set(mappedData)]
+    console.log(uniqueValues)
+
+    select = document.querySelector("#selectDovolnost");
+    console.log(select)
+
+    uniqueValues.forEach(item => {
+        let option = document.createElement("option"); 
+
+        option.value = item; 
+        option.text = item;
+
+        select.add(option) 
+    })
+
+    //----------------------------
+
+    mappedData = data.map(item => {
+        return item['Отметь, в какой мере ты удовлетворен курсом?'];
+    })
+    console.log(mappedData)
+
+    uniqueValues = [...new Set(mappedData)]
+    console.log(uniqueValues)
+
+    select = document.querySelector("#selectUdovletvoronost");
+    console.log(select)
+
+    uniqueValues.forEach(item => {
+        let option = document.createElement("option"); 
+
+        option.value = item; 
+        option.text = item;
+
+        select.add(option) 
     })
 }
 
