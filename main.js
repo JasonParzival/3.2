2ba2cb70a31235d40301fbe13649613b552ec982
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-10 21:35:36 +0800

    Разобрался с подсказкой, иду пилить задачу

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ async function process() {
 
     let uniqueValues = [...new Set(mappedData)]
     console.log(uniqueValues)
+
+    let select = document.querySelector("#selectPoleznost");
+    console.log(select)
+
+    uniqueValues.forEach(item => {
+        // создаем в памяти элемент выпадающего списка
+        let option = document.createElement("option"); 
+
+        option.value = item;  // устанавливаем значение элемента
+        option.text = item; // устанавливаем текст элемента, он совпадает со значением
+
+        select.add(option) // добавляем элемент к выпадающему списку
+    })
 }
 
 process()
