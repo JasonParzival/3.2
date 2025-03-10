33a29b5826d6fdb5e085823d9464f37188439af6
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-10 22:19:32 +0800

    Небольшие изменения

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 async function process() {
     let r = await fetch("/data.json");
     let data = await r.json();
-    console.log(data) 
     
     let mappedData = data.map(item => {
         return item['Насколько курс был полезен?'];
     })
-    console.log(mappedData)
 
     let uniqueValues = [...new Set(mappedData)]
-    console.log(uniqueValues)
 
     let select = document.querySelector("#selectPoleznost");
-    console.log(select)
 
     uniqueValues.forEach(item => {
         let option = document.createElement("option"); 
@@ -28,13 +24,10 @@ async function process() {
     mappedData = data.map(item => {
         return item['Насколько доволен форматом обучения?'];
     })
-    console.log(mappedData)
 
     uniqueValues = [...new Set(mappedData)]
-    console.log(uniqueValues)
 
     select = document.querySelector("#selectDovolnost");
-    console.log(select)
 
     uniqueValues.forEach(item => {
         let option = document.createElement("option"); 
@@ -50,13 +43,10 @@ async function process() {
     mappedData = data.map(item => {
         return item['Отметь, в какой мере ты удовлетворен курсом?'];
     })
-    console.log(mappedData)
 
     uniqueValues = [...new Set(mappedData)]
-    console.log(uniqueValues)
 
     select = document.querySelector("#selectUdovletvoronost");
-    console.log(select)
 
     uniqueValues.forEach(item => {
         let option = document.createElement("option"); 
